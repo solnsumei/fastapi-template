cf6b8c8583a17ab9189b7198c41b565225b302a1
solnsumei <solnsumei@gmail.com>
2020-12-22 23:08:29 +0100

    Add role schema

---
+++ src/models/schema/role.py
@@ -0,0 +1,8 @@
+from typing import Optional
+from pydantic import Field
+from .baseschema import BaseSchema
+
+
+class RoleSchema(BaseSchema):
+    role: str = Field(..., min_length=2, max_length=15)
+    permission: Optional[str] = None
