f968bc4e747c03ff0ca1af7324825b76009ecc9e
solnsumei <solnsumei@gmail.com>
2020-12-22 23:16:43 +0100

    Attach roles router to app

--- src/routes.py
+++ src/routes.py
@@ -1,4 +1,6 @@
-from src.api import auth
+from fastapi import Depends
+from src.api import auth, roles
+from src.utils.security import get_current_user
 
 
 def add_routers(app, config):
@@ -8,8 +10,16 @@ def add_routers(app, config):
     :param config:
     :return: None
     """
+
     app.include_router(
         auth.router,
         prefix=f"{config.API_URL}/auth",
         tags=["Authentication"]
+    )
+
+    app.include_router(
+        roles.router,
+        prefix=f"{config.API_URL}/roles",
+        tags=["Roles"],
+        dependencies=[Depends(get_current_user)]
     )
