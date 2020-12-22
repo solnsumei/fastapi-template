d0c98bcd0d7445f497ee4c6778839996ac0a61d7
solnsumei <solnsumei@gmail.com>
2020-12-22 23:13:14 +0100

    Add roles router

---
+++ src/api/roles.py
@@ -0,0 +1,10 @@
+from .baserouter import BaseRouter
+from src.models import Role, RolePydantic
+from src.models.schema.role import RoleSchema
+
+
+router = BaseRouter(
+    model=Role,
+    request_schema=RoleSchema,
+    response_schema=RolePydantic
+)
